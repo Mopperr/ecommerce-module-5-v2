1eeaa1aecabc709e6fd76f5ff04cae4461d5a07c
and87 <and87@example.com>
2026-02-04 17:44:20 -0500

    Initial commit: ecommerce module 5 project

---
+++ library-react-tutorial/src/components/Featured.jsx
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import Book from "./ui/Book";
+import { books } from "../data";
+import { Link } from "react-router-dom"
+
+const Featured = () => {
+  console.log(books);
+  console.log();
+  return (
+    <section id="features">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Featured <span className="purple">Books</span>
+          </h2>
+          <div className="books">
+            {books
+            .filter((book) => book.rating === 5)
+            .slice(0, 4)
+            .map((book) =>  (
+              <Book book={book} key={book.id} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Featured;
